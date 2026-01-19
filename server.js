c1416722b53bf990768833e5c8cab9c14c859bf2
tuplancontacto-lgtm <tuplancontacto@gmail.com>
2026-01-19 14:28:06 -0300

    Refactor server.js for improved structure and clarity
    Reseteado del 7 enero

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ async function connectDB() {
 w: 'majority'
 });
 
-    await global.client.connect();
+    await client.connect();
     db = global.client.db('colmena');
     asesorCollection = db.collection('asesores');
     actividadCollection = db.collection('actividad');
@@ -449,8 +449,6 @@ connectDB().then(() => {
     console.log(`📊 Panel Admin Asesores: http://localhost:${PORT}/admin-asesores.html`);
   });
 });
-
-
 
 
 
